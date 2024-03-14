87d42016998a90d727306bc260200f9e4591c15a
Justin <justinguerrerowork@gmail.com>
2024-03-14 17:09:16 -0400

    basic templates

---
+++ website/auth.py
@@ -0,0 +1,16 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+
+auth = Blueprint("auth", __name__)
+
+@auth.route("/login", methods = ["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+@auth.route("/sign-up", methods = ["GET", "POST"])
+def sign_up():
+    return render_template("signup.html")
+
+@auth.route("/logout")
+def logout():
+    return redirect(url_for("views.home"))
+
